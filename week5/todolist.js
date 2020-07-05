ed2bb3b89da430974757f70d920199504c3d3f33
BrenSeibel <brens.seibel@gmail.com>
2020-07-05 15:27:33 -0400

    working

--- week5/todolist.js
+++ week5/todolist.js
@@ -1,90 +1,115 @@
-// var myNodelist = document.getElementsByTagName("LI");
-// var i;
-// for (i = 0; i < myNodelist.length; i++) {
-//   var span = document.createElement("SPAN");
-//   var txt = document.createTextNode("\u00D7");
-//   span.className = "close";
-//   span.appendChild(txt);
-//   myNodelist[i].appendChild(span);
-// }
+var myNodelist = document.getElementsByTagName("LI");
+var i;
+for (i = 0; i < myNodelist.length; i++) {
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  myNodelist[i].appendChild(span);
+}
 // document.getElementsByClassName("addBtn")[0]
 // .addEventListener("click", ()=>{
-// newElement("myInput")
-// newElement("myInput2")
+// newElement()
+
 // })
 
-// var close = document.getElementsByClassName("close");
-// var i;
-// for (i = 0; i < close.length; i++) {
-//   close[i].onclick = function() {
-//     var div = this.parentElement;
-//     div.style.display = "none";
-//   }
-// }
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
+  }
+}
 
-// var list = document.getElementById('myUL');
-// list.addEventListener('click', function(ev) {
-//   console.log(ev.target.tagName === 'LI')
-//   if (ev.target.tagName === 'LI') {
-//     ev.target.classList.toggle('checked');
-//   }
-// }, false);
+var list = document.getElementById('myUL');
+list.addEventListener('click', function(ev) {
+  console.log(ev.target.tagName === 'LI')
+  if (ev.target.tagName === 'LI') {
+    ev.target.classList.toggle('checked');
+  }
+}, false);
 
-// function newElement(inputId) {
-//   var li = document.createElement("li");
-//   var inputValue = document.getElementById(inputId).value;
-//   var t = document.createTextNode(inputValue);
-//   li.appendChild(t);
-//   if (inputValue === '') {
-//     alert("You must write something!");
-//   } else {
-//     document.getElementById("myUL").appendChild(li);
-//   }
-//   document.getElementById(inputId).value = "";
+function newElement(dataObject) {
+    
+const todoForm = document.todoform;
 
-//   var span = document.createElement("SPAN");
-//   var txt = document.createTextNode("\u00D7");
-//   span.className = "close";
-//   span.appendChild(txt);
-//   li.appendChild(span);
+    
+  var li = document.createElement("li");
 
-//   for (i = 0; i < close.length; i++) {
-//     close[i].onclick = function() {
-//       var div = this.parentElement;
-//       div.style.display = "none";
-//     }
-//   }
-// }
+  li.textContent = dataObject.title;
+  if (document.todoform.title === '') {
+    alert("You must write something!");
+  } else {
+    document.getElementById("myUL").appendChild(li);
+  }
+  document.todoform.title = "";
 
-const todoForm = document.todofrom;
+  var li2 = document.createElement("li");
+
+  li2.textContent = dataObject.description;
+  if (document.todoform.description === '') {
+    alert("You must write something!");
+  } else {
+    document.getElementById("myUL").appendChild(li2);
+  }
+  document.todoform.description = "";
+  
+  
+  var li3 = document.createElement("li");
+
+  li3.textContent = dataObject.price;
+  if (document.todoform.price === '') {
+    alert("You must write something!");
+  } else {
+    document.getElementById("myUL").appendChild(li3);
+  }
+  document.todoform.price = "";
+
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
+
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      var div = this.parentElement;
+      div.style.display = "none";
+    }
+  }
+}
+
+const todoForm = document.todoform;
 
 todoForm.addEventListener("submit", function(event){
     event.preventDefault()
-
+    const todoForm = document.todoform;
     const newTodo = {
-        title : todoForm.tittle.value,
+        title : todoForm.title.value,
         description : todoForm.description.value,
         price : todoForm.price.value
     }
-    axios.post("https://api.vaschool.io/BrenSeibel/todo", newTodo)
-    .then(response => console.log(response.data))
+    axios.post("https://api.vschool.io/<BrenSeibel>/todo", newTodo)
+    .then(response => {console.log(response.data)
+    newElement(response.data)})
     .catch(error => console.log(error))
 })
 
 console.log(axios)
 
 
-const checkbox = document.createElement("input")
-checkbox.type = "checkbox"
-document.body.appendChild(checkbox)
+// const checkbox = document.createElement("input")
+// checkbox.type = "checkbox"
+// document.body.appendChild(checkbox)
 
-checkbox.addEventListener("change", function(event){
-    console.log(event.target.checked)
+// checkbox.addEventListener("change", function(event){
+//     console.log(event.target.checked)
     
-    axios.put("https://api.vschool.io/BrenSeibel/todo")+
-    (arr[i]._id), {completed: event.targer.checked}
-    .then(response => {console.log(response.data)
+//     axios.put("https://api.vschool.io/BrenSeibel/todo")+
+//     (arr[i]._id), {completed: event.targer.checked}
+//     .then(response => {console.log(response.data)
     
-    })
-    .catch(error => console.log(error))
-});
+//     })
+//     .catch(error => console.log(error))
+// });
